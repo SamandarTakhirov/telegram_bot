0d96efcfb80d506dfaa320c9a6e323ae70205806
SamandarTakhirov <itbloog741@gmail.com>
2025-01-29 14:15:24 +0500

    done

--- lib/constants/const_keys.dart
+++
@@ -1,9 +0,0 @@
-// class ConstKeys {
-//   static const botToken = '7571666941:AAHzNLw4rOo2KDK91yqlTJNjc41nKPiOu4U';
-//   // static const botToken = '8123558993:AAEfY-yFDZua5uHvyoWNCLV7TXJ2YCFFXfw';
-//   static const bankUrl = 'https://cbu.uz/uz/arkhiv-kursov-valyut/json/';
-//   static const urlWeather = 'https://api.openweathermap.org/data/2.5/weather';
-//   static const urlImage = 'https://api.unsplash.com/search/photos';
-//   static const apiWeather = 'ca76dc89ac54c97d368986a0a5351e73';
-//   static const imageApiKey = 'e5MrEKuWIryMWb8nDztpeCtCLORK55WSumh0dgl_N-8';
-// }
